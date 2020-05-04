b4c132fc9550404ad3c3d3c3438f9536c8a1b89a
v1rtl <pilll.PL22@gmail.com>
2020-05-04 12:33:01 +0300

    getting the function back to the same behaviour

--- mdx.d.ts
+++ mdx.d.ts
@@ -1,4 +1,8 @@
 declare module '*.mdx' {
   let MDXComponent: (props: any) => JSX.Element
+  export const title: string
+  export const date: string
+  export const image: string
+  export const desc: string
   export default MDXComponent
 }
