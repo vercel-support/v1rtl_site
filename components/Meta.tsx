af19687545070ad4c0e435e7a30f8240dca37199
v1rtl <pilll.PL22@gmail.com>
2020-05-05 12:30:41 +0300

    improve mdx headings

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -8,16 +8,20 @@ type Meta = {
   topic?: string
 }
 
-const Meta = ({ title, desc, image, topic = 'Programming' }: Meta) => (
-  <Head>
-    <title>{title}</title>
-    <meta name="og:title" content={title} />
-    <meta name="og:type" content="article" />
-    <meta name="og:description" content={desc} />
-    <meta name="og:image" content={image} />
-    <meta name="og:article:section" content={topic} />
-    <meta name="og:article:author" content="v1rtl" />
-  </Head>
+const Meta = ({ meta }: { meta: Meta }) => (
+  <>
+    <Head>
+      <title>{meta.title}</title>
+      <meta name="og:title" content={meta.title} />
+      <meta name="og:type" content="article" />
+      <meta name="og:description" content={meta.desc} />
+      <meta name="og:image" content={meta.image} />
+      <meta name="og:article:section" content={meta.topic} />
+      <meta name="og:article:author" content="v1rtl" />
+    </Head>
+    <img src={meta.image} alt={meta.title} />
+    <h1>{meta.title}</h1>
+  </>
 )
 
 export default Meta
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -174,14 +174,17 @@ const CustomApp = ({ Component, pageProps }: AppProps) => {
         <article
           css={{
             padding: '3rem',
+            paddingTop: '5rem',
             width: 'calc(40vw + 10em)',
             margin: '0 auto',
             p: {
               textAlign: 'justify',
             },
             img: {
+              height: '350px',
               width: '100%',
               margin: '1em 0',
+              objectFit: 'cover',
             },
             h1: {
               textAlign: 'center',
