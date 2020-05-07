f5567d63432c54ea1f1e13472492fbe305a764d9
v1rtl <pilll.PL22@gmail.com>
2020-05-07 16:14:51 +0300

    include polyfill

--- next.config.js
+++ next.config.js
@@ -19,17 +19,17 @@ module.exports = withOptimizedImages(
       pageExtensions: ['md', 'mdx', 'tsx', 'ts'],
 
       webpack(config) {
-        // const originalEntry = config.entry
+        const originalEntry = config.entry
 
-        // config.entry = async () => {
-        //   const entries = await originalEntry()
+        config.entry = async () => {
+          const entries = await originalEntry()
 
-        //   if (entries['main.js'] && !entries['main.js'].includes('./client/polyfills.js')) {
-        //     entries['main.js'].unshift('./client/polyfills.js')
-        //   }
+          if (entries['main.js'] && !entries['main.js'].includes('./client/polyfills.js')) {
+            entries['main.js'].unshift('./client/polyfills.js')
+          }
 
-        //   return entries
-        // }
+          return entries
+        }
 
         /* GLSL */
 
